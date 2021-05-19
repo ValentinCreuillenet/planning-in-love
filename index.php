591a988fabf9dfb0a4a75a9866066efefc639371
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-05-19 10:10:56 +0000

    implemented header and footer

--- index.php
+++ index.php
@@ -3,23 +3,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
     <title>Planning in Love</title>
 </head>
-<body>
-    
-</body>
-</html>
-
-
 <?php
     include("./template/header.php");
 
-    include("./template/navigation.php");
-
-    include("./pages/{$_GET["folder"]}/{$_GET["file"]}.php");
+    include("./template/main.php");
 
     include("./template/footer.php");
-
-    include("./template/footer.php");
-
 ?>
+</html>
+
+
---
+++ template/footer.php
@@ -0,0 +1,26 @@
+<footer class="bg-white border-t border-gray-400 shadow">
+		<div class="container max-w-4xl mx-auto flex py-8">
+
+			<div class="w-full mx-auto flex flex-wrap">
+				<div class="flex w-full md:w-1/2 ">
+					<div class="px-8">
+						<h3 class="font-bold text-gray-900">About</h3>
+						<p class="py-4 text-gray-600 text-sm">
+							Planning in love est le fruit du génie du grand gourou Loïc Derrieux.
+						</p>
+					</div>
+				</div>
+
+				<div class="flex w-full md:w-1/2">
+					<div class="px-8">
+						<h3 class="font-bold text-gray-900">Social</h3>
+						<ul class="list-reset items-center text-sm pt-3">
+							<li>
+								<a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="https://fondespierre.com/nos-poles-de-competences/beweb-ecole-numerique/developpeur-web/" target="_blank">Fondespierre</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
