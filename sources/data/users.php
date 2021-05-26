6c9726c22df221ff2f42649e4db53f3ebf1e73d7
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-05-26 22:15:43 +0000

    get user info

--- sources/data/users.php
+++ sources/data/users.php
@@ -3,31 +3,70 @@
 include("./sources/data/PDO.php");
 
 
+/*
+function getFriendCards($offset,$pdo,$id){
 
-function getUsercards($offset,$pdo){
+  $friends=[];
 
-  $users=[];
-
-  $query = "SELECT firstname, lastname, mail, logo, id FROM user LIMIT {$offset},50";
+  $query = "SELECT user.firstname, user.lastname, user.mail, user.logo, user.id FROM user_team
+            LIMIT {$offset},50
+            INNER JOIN user ON user.id = user_team.user
+            WHERE user_team.id = $id";
   $stmt = $pdo->prepare($query);
   $stmt->execute();
 
-  foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user) {
-    array_push($users,$user);
+  foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $friend) {
+    array_push($friends,$friend);
   }
 
-  return $users;
+  return $friends;
 
 }
+*/
 
+
+/* Requete imbriquée : 
+select USER.logo, USER.id , USER.username , USER_TEAM.team FROM USER_TEAM
+
+inner join USER ON USER.id = USER_TEAM.user
+
+where team in (select team from USER_TEAM where user = 21) and user != 21
+*/
+
+//
+function getUserInfo($username,$password,$pdo){
+
+    $query = "SELECT firstname, logo, id FROM user WHERE user.username LIKE {$username} AND user.password LIKE {$password}";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+}
+
+//Retourne les informations de 50 utilisateurs a partir d'un offset pour l'affichage via liste
+function getUserCards($offset,$pdo){
+
+    $users=[];
+
+    $query = "SELECT firstname, lastname, mail, logo, id FROM user LIMIT {$offset},50";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+
+    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user) {
+        array_push($users,$user);
+    }
+
+    return $users;
+}
+
+//Retourne les information complète d'un utilisateur via son ID
 function getUserById($id,$pdo){
 
-  $query = "SELECT * FROM user WHERE id=$id";
-  $stmt = $pdo->prepare($query);
-  $stmt->execute();
+    $query = "SELECT * FROM user WHERE id={$id}";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
 
-  return $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
-
+    return $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 }
 
 
--- template/header.php
+++ template/header.php
@@ -55,7 +55,7 @@
 		</div>
 
 		<div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
-			<ul class="list-reset lg:flex justify-end flex-1 items-center">
+			<ul class="list-reset lg:flex justify-start flex-1 items-center">
 
 			<?= generateNavbar() ?>
 			
